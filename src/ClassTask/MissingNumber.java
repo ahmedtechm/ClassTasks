6bb651114a9e6adaa19a78b158888d4c868cdb28
ahmedtechm <ahmedtechm2050@gmail.com>
2023-01-17 11:07:32 +0400

    adding Missing Number Task

---
+++ src/ClassTask/MissingNumber.java
@@ -0,0 +1,18 @@
+package ClassTask;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		
+
+		int[] array = { 1, 3, 4, 5, 7, 8 };
+
+		for(int i = 0; i < array.length-1; i++){
+    
+			if(array[i+1] != array[i] + 1){
+        
+    	System.out.println("Missing Number is : " + (array[i] + 1));
+			}
+		}
+	}
+}
